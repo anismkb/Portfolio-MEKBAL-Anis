71429754494424104c0173b64e9ac401887f9030
Anis Mekbal <anismekbal2001@gmail.com>
2025-12-02 13:26:35 +0100

    style the project

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Project.css";
+import ProjectCard from "./ProjectCard.jsx";
+import conveydyn from '../../assets/convey.jpg';
 
 const Projet = () => {
   return (
@@ -7,7 +9,32 @@ const Projet = () => {
       <div className="title">
         <h1>Projects</h1>
         <p className="subtitle">My recents projects</p>
-        <hr class="blue-line" />
+        <hr className="blue-line" />
+      </div>
+      <div className="projectCards">
+        <ProjectCard 
+          image= {conveydyn}
+          lien={""}
+          title="Conveydyn Wizard"
+          description="It is used to help with the sizing of a Conveydyn® belt for roller conveyors — whether straight or curved conveyors."
+          items={[
+            { technologie: "Flutter" },
+            { technologie: "Dart" },
+            { technologie: "Asp.Net" },
+          ]}
+        />
+
+        <ProjectCard 
+          image= {conveydyn}
+          lien={""}
+          title="Conveydyn Wizard"
+          description="It is used to help with the sizing of a Conveydyn® belt for roller conveyors — whether straight or curved conveyors."
+          items={[
+            { technologie: "Flutter" },
+            { technologie: "Dart" },
+            { technologie: "Asp.Net" },
+          ]}
+        />
       </div>
     </div>
   );
--- src/Components/Project/ProjectCard.jsx
+++ src/Components/Project/ProjectCard.jsx
@@ -1,27 +1,30 @@
+export default function ProjectCard({ image, lien, title, description, items }) {
+  return (
+    <div className="card-skills">
+      <img src={image} alt={title} />
 
-export default function SkillCard({ image, lien, title, description, items }) {
-    return(
-        <div className="card">
-            <div className="image">
-                <img src={image} alt="" />
-                <div className="title"> 
-                    <h2>{title}</h2>     
-                </div>
-                <div className="description">
-                    <p> {description} </p>
-                </div>
-                <div className="items">
-                    {items.map((i, index)=>(
-                        <dev key={index} className="item">
-                            <p> {i.technologie} </p>
-                        </dev>
-                    ))
-                    }
-                </div>
-                <div className="lien">
-                    <a href={lien} target="_blank"> View project </a>
-                </div>
-            </div>
-        </div>
-    );
+      <div className="title">
+        <br />
+        <h2>{title}</h2>
+      </div>
+
+      <div className="description">
+        <p>{description}</p>
+      </div>
+
+      <div className="items">
+        {items.map((i, index) => (
+          <div key={index} className="item">
+            <p>{i.technologie}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="lien">
+        <a href={lien} target="_blank" rel="noreferrer">
+          View project
+        </a>
+      </div>
+    </div>
+  );
 }
